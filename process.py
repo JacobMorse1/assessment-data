39b95e3407e8f949d611185ff678ac51282ffebf
Jacob Morse <jacobsmorse@gmail.com>
2022-03-11 09:58:39 -0700

    initial commit

---
+++ process.py
@@ -0,0 +1,24 @@
+# This line is opening the .txt file and setting it
+# equal the variable log_file. This allows us to work with the info 
+# stored in the .txt file in process.py
+
+log_file = open("um-server-01.txt") 
+
+# This line is a function called sales_reports that takes in the log_file 
+# as a parameter
+
+def sales_reports(log_file):
+    # This line is a for loop where we are looping over log_file
+    for line in log_file:
+        # This line is removing any space on the right side of the string
+        line = line.rstrip()
+        # This line is setting the variable 'day' equal to every 3rd index 
+        # starting from the 0 index
+        day = line[0:3]
+        # This conditional states that if the value of the above indicated index
+        # contains the specified string then print the full line
+        if day == "Mon":
+            print(line)
+
+# Here the function is called and log_file is passed in as a param
+sales_reports(log_file)
